0500f24b12dd876251d9afcd7b643284eedee6d0
doenervich <admin@thurnax.com>
2013-12-04 20:43:41 +0100

    Create TourPointListPage.class.php

---
+++ lib/acp/page/TourPointListPage.class.php
@@ -0,0 +1,44 @@
+<?php
+namespace wcf\acp\page;
+use wcf\page\SortablePage;
+
+/**
+ * Lists available tour points.
+ * 
+ * @author	Simon Nußbaumer
+ * @copyright	2013 Thurnax.com
+ * @package	com.thurnax.wcf.tour
+ * @subpackage	acp.page
+ * @category	Community Framework (commercial)
+ */
+class TourPointListPage extends SortablePage {
+	/**
+	 * @see	\wcf\acp\page\AbstractPage::$activeMenuItem
+	 */
+	public $activeMenuItem = 'wcf.acp.menu.link.tour.tourPoint.list';
+	
+	/**
+	 * @see	\wcf\page\AbstractPage::$neededPermissions
+	 */
+	public $neededPermissions = array('admin.tour.canEdit');
+	
+	/**
+	 * @see	\wcf\page\AbstractPage::$neededModules
+	 */
+	public $neededModules = array('MODULE_TOUR');
+	
+	/**
+	 * @see	\wcf\page\MultipleLinkPage::$objectListClassName
+	 */
+	public $objectListClassName = 'wcf\data\tour\tourPoint\TourPointList';
+	
+	/**
+	 * @see	\wcf\page\MultipleLinkPage::$defaultSortField
+	 */
+	public $defaultSortField = 'step';
+	
+	/**
+	 * @see	\wcf\page\MultipleLinkPage::$validSortFields
+	 */
+	public $validSortFields = array('tourPointID', 'step', 'elementName', 'pointText', 'position');
+}
