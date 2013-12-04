87b5f34ec4e5afa26089426d1d7241a9c3bf8b3a
doenervich <admin@thurnax.com>
2013-12-04 20:44:32 +0100

    Create TourPointAction.class.php

---
+++ lib/data/tour/tourPoint/TourPointAction.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace wcf\data\tour\tourPoint;
+use wcf\data\AbstractDatabaseObjectAction;
+
+/**
+ * Executes tour point.
+ * 
+ * @author	Simon Nußbaumer
+ * @copyright	2013 Thurnax.com
+ * @package	com.thurnax.wcf.tour
+ * @subpackage	data.tour.point
+ * @category	Community Framework (commercial)
+ */
+class TourPointAction extends AbstractDatabaseObjectAction {
+	/**
+	 * @see	\wcf\data\AbstractDatabaseObjectAction::$className
+	 */
+	protected $className = 'wcf\data\tour\tourPoint\TourPointEditor';
+	
+	/**
+	 * @see	\wcf\data\AbstractDatabaseObjectAction::$permissionsDelete
+	 */
+	protected $permissionsDelete = array('admin.tour.canEdit');
+	
+	/**
+	 * @see	\wcf\data\AbstractDatabaseObjectAction::$permissionsUpdate
+	 */
+	protected $permissionsUpdate = array('admin.tour.canEdit');
+	
+	/**
+	 * @see	\wcf\data\AbstractDatabaseObjectAction::$requireACP
+	 */
+	protected $requireACP = array('delete', 'update');
+}
