9bbfc75c5ff65924506ee2f3753cfe5a602f8cfa
doenervich <admin@thurnax.com>
2013-12-04 20:45:14 +0100

    Create TourPointList.class.php

---
+++ lib/data/tour/tourPoint/TourPointList.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace wcf\data\tour\tourPoint;
+use wcf\data\DatabaseObjectList;
+
+/**
+ * Represents a list of tour points.
+ * 
+ * @author	Simon Nußbaumer
+ * @copyright	2013 Thurnax.com
+ * @package	com.thurnax.wcf.tour
+ * @category	Community Framework (commercial)
+ */
+class TourPointList extends DatabaseObjectList {
+	/**
+	 * @see	\wcf\data\DatabaseObjectList::$className
+	 */
+	public $className = 'wcf\data\tour\tourPoint\TourPoint';
+}
