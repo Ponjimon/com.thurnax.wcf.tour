9add6be47749643fee96fe564780f74fe2ce808b
Magnus Kühn <magnus.khn@gmail.com>
2014-01-05 20:43:44 +0100

    Updated update statements and script

--- files/acp/update_com.thurnax.wcf.tour_1.0.0_rc_2.php
+++ files/acp/update_com.thurnax.wcf.tour_1.0.0_rc_2.php
@@ -13,34 +13,9 @@ use wcf\system\WCF;
  * @package	com.thurnax.wcf.tour
  */
 
-// update wcf1_tour
-executeQuery("ALTER TABLE wcf1_tour ADD COLUMN packageID INT(10) NOT NULL");
-executeQuery("ALTER TABLE wcf1_tour MODIFY tourTrigger ENUM('firstSite', 'specificSite', 'manual') NOT NULL DEFAULT 'firstSite'");
-
-// update wcf1_tour_step
-executeQuery("ALTER TABLE wcf1_tour_step ADD COLUMN packageID INT(10) NOT NULL");
-executeQuery("ALTER TABLE wcf1_tour_step MODIFY placement ENUM('top', 'bottom', 'left', 'right') NOT NULL DEFAULT 'left'");
-executeQuery("ALTER TABLE wcf1_tour_step MODIFY xOffset INT(10) NOT NULL DEFAULT 0");
-executeQuery("ALTER TABLE wcf1_tour_step MODIFY yOffset INT(10) NOT NULL DEFAULT 0");
-executeQuery("ALTER TABLE wcf1_tour_step ADD COLUMN ctaLabel VARCHAR(255) NULL DEFAULT NULL");
-executeQuery("ALTER TABLE wcf1_tour_step ADD COLUMN onPrev MEDIUMTEXT NULL DEFAULT NULL");
-executeQuery("ALTER TABLE wcf1_tour_step ADD COLUMN onNext MEDIUMTEXT NULL DEFAULT NULL");
-executeQuery("ALTER TABLE wcf1_tour_step ADD COLUMN onShow MEDIUMTEXT NULL DEFAULT NULL");
-executeQuery("ALTER TABLE wcf1_tour_step ADD COLUMN onCTA MEDIUMTEXT NULL DEFAULT NULL");
-
 // set package id
 setPackageID(Tour::getDatabaseTableName());
 setPackageID(TourStep::getDatabaseTableName());
-
-/**
- * Executes a simple query
- * 
- * @param	string	$sql
- */
-function executeQuery($sql) {
-	$statement = WCF::getDB()->prepareStatement($sql);
-	$statement->execute();
-}
 
 /**
  * Sets the package id in a table
